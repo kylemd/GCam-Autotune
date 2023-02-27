2a7677f9b40a2c86d786b5459cc16b507db65675
kylemd <kyle.m.drayton@gmail.com>
2023-02-27 13:07:19 +1100

    Start making changes to pipeline to patch library file rather than in RAM (thanks UltraM8 for pointing out some values are not read after camera init)
    This is a breaking change as it changes how the whole pipeline works and is not yet finished.

--- pipeline.py
+++ pipeline.py
@@ -3,29 +3,38 @@ import time
 import ops_ADB as ctrl
 import ops_FridaPatcher as patch
 import ops_Vision as iqa
-
-
+from ProjectPepega import arm as hex2arm
 
 # Start device
 
-def initialise_device(package,activity):
-    device = ctrl.ConnectDevice()
+def initialise_camera(device,package,activity):
     ctrl.StartCamera(device,package,activity)
     time.sleep(1)
-    patchscript = patch.load_hook(package)
     cam_running = ctrl.CheckAppRunning(device,package)
 
     if cam_running == True:
-        return device, patchscript
+        return device
     else:
         return 0, 0
 
+def patch_libs(device,libpath,tunable,newvalue):
+    address = tunable['address']
+    hexvalue = hex2arm.generate_hex(tunable['hex_original'], newvalue)
+
+    if hexvalue == None:
+        hexvalue = hex(int(newvalue))
+    else:
+        hexvalue = swap_endianness(hexvalue.upper())
+
+    device.shell(["python","/sdcard/autotune_patcher.py",libpath,address,hexvalue])
+
 def generate(device,patchscript,package,directory,format,tunable,newvalue,metric):      #Passing vars here?
     try:
-        time.sleep(1)
-        hexnew = patch.PatchRAM(patchscript,tunable,newvalue)
+        libpath = '/sdcard/libgcastartup.so'
+        patch_libs(libpath,tunable,newvalue)
+        initialise_camera(device,package,activity)
         #device.click(((2340/2)-150), (1080/2))
-        time.sleep(1)
+        time.sleep(3)
         ctrl.TakePhoto(device)
         file = ctrl.WaitForNewFile(device, directory, format, package)
         localfile = ctrl.RetrievePhoto(device,file,directory,format)
