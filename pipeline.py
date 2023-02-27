063acfeae411c404e3478c2dcc670be22f8a59d2
kylemd <kyle.m.drayton@gmail.com>
2023-02-27 17:07:02 +1100

    Simplify - Start moving IQA functions over to pipeline

--- pipeline.py
+++ pipeline.py
@@ -17,6 +17,11 @@ def initialise_camera(device,package,activity):
     else:
         return 0, 0
 
+def initialise_iqa(metric):
+	iqa_metric = pyiqa.create_metric(metric, device=torch.device('cuda'))
+	metric_direction = iqa_metric.lower_better
+	return iqa_metric, metric_direction
+
 def patch_libs(device,libpath,tunable,newvalue):
     address = tunable['address']
     hexvalue = hex2arm.generate_hex(tunable['hex_original'], newvalue)
@@ -44,3 +49,26 @@ def generate(device,patchscript,package,directory,format,tunable,newvalue,metric
 
     except Exception:
         return Exception, Exception, Exception
+
+def IQTest(image,metric):
+
+	test_image = cv2.imread(image)
+
+	gray = cv2.cvtColor(test_image, cv2.COLOR_BGR2GRAY)	
+	gaussian = cv2.GaussianBlur(gray,(3,3),cv2.BORDER_DEFAULT)
+	edges = cv2.Canny(gaussian,100,200)
+	contours, hierarchy = cv2.findContours(edges,cv2.RETR_TREE,cv2.CHAIN_APPROX_NONE)
+	contour = max(contours, key = len)
+		
+	x, y, w, h = cv2.boundingRect(contour)
+
+	# Py-IQA module method
+	
+	# Display cropped image
+	
+	cropped=test_image[y:y+h,x:x+w]
+	cropped_tensor=torch.tensor(cropped).permute(2,0,1)[None,...]/255.
+	score_nr = metric(cropped_tensor).item()
+	
+	cv2.imwrite(image, cropped)
+	return score_nr
