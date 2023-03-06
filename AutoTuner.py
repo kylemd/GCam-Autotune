d7a0d3af001f7286b972a3a24f195d50defe287a
kylemd <kyle.m.drayton@gmail.com>
2023-03-06 22:46:47 +1100

    Remove double initialisation of iqa model Add list of NR IQA models for easy testing

--- AutoTuner.py
+++ AutoTuner.py
@@ -11,25 +11,28 @@ args_dict = {
                   "/libgcastartup.so",
     "remoteOutputDir": "/sdcard/DCIM/Camera",
     "remoteOutputExt": "jpg",
-    "iqaMethod": "pi",
+    "iqaMethod": "brisque",
     "outputDir": ".\\output"
 }
 
+# >>> print(pyiqa.list_models("NR")) 
+# ['brisque', 'clipiqa', 'clipiqa+', 'cnniqa', 'dbcnn', 'fid', 'ilniqe', 'maniqa', 'musiq', 'musiq-ava', 'musiq-koniq', 'musiq-paq2piq', 'musiq-spaq', 'nima', 
+#  'nima-vgg16-ava', 'niqe', 'nrqm', 'paq2piq', 'pi', 'tres', 'tres-flive', 'tres-koniq']
+
 # Copy and paste a value from Rivovs API here if you want to test just one specific value
 testParam = [{
-    "id": 106,
-    "name": "Sharp gain",
+    "id": 775,
+    "name": "",
     "lib_version": "8.4.400_rc19",
-    "arm_type": "ARMFLT",
-    "disasm": "movz w8, #0xbf80, lsl #16",
-    "added_on": "2022-07-01T09:39:05.875276",
+    "arm_type": "ARMRPL",
+    "disasm": "fdiv s9, s1, s10",
+    "added_on": "2022-07-12T12:20:05.566349",
     "description": "",
-    "address": "01FBC294",
+    "address": "01ED2E58",
     "length_in_lib": 4,
-    "hex_original": "08F0B752",
-    "extracted_value": "-1",
-    "added_by": 1,
-    "range": [0, 65535]
+    "hex_original": "29182A1E",
+    "extracted_value": "",
+    "range": [ -31, 31 ]
 }]
 
 device = Ctrl.connect_device()
@@ -44,7 +47,5 @@ if len(testParam) != 0:
     lib_dict = testParam
 else:
     lib_dict = LibAPI.get_lib_values()
-
-iqaMetric, metricDirection = ops_Pipeline.initialise_iqa(args_dict['iqaMethod'])
 
 Experiment.run_optimisation(device, args_dict, lib_dict)
